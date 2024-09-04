7883a0f4de18d176953d2dc7e168643b6377ee3b
CosmeNicolas <nicomas7590@gmail.com>
2024-09-04 20:29:40 -0300

    agregue los iconos para el footer y linkie los enlaces para las redes

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,52 +1,58 @@
 import React from 'react'
-import { BsTwitterX,BsFacebook, BsInstagram,  BsLinkedin, BsHeartFill} from "react-icons/bs";
-import { BsFillThreadsFill } from "react-icons/bs";
+import { BsFacebook, BsInstagram,  BsLinkedin, BsHeartFill} from "react-icons/bs";
 import { Link } from 'react-router-dom';
+import { FaWhatsapp } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaStoreAlt } from "react-icons/fa"; //tienda icono
 const Footer = () => {
   return (
-    <footer className="bg-negroMate py-4 font-oswald  flex flex-col md:flex-row md:justify-around ">
-    <article className="text-center">
-      <div className="bg-azul-oscuro rounded-xl p-4">
+    <footer className="bg-negroMate  ">
+  <article className="text-center">
+     Donde estamos ? 
+     <FaLocationDot />
+     <p> Av. Avellaneda 3142</p>
+     <FaStoreAlt /><p>www.luzbell.com.ar</p>
+    </article> 
+    <article className=''>
+      
+        <p className="text-lg text-blue-50 font-oswald font-bold mx-1">
+          Sigueme
+        </p>
+        <ul className="flex  justify-between   text-slate-200 mt-1">
+          <Link to="https://www.tiktok.com/@luzbell122?lang=es">
+            <li>
+              <FaTiktok className="text-[#000] mx-1"/>
+            </li>
+          </Link>
+          <Link to="https://www.facebook.com/luz.bellavellaneda.5">
+            <li>
+              <BsFacebook className="text-[#1877F2] mx-1" />
+            </li>
+          </Link>
+          <Link to='https://www.instagram.com/luzbellavellaneda/'>
+            <li>
+              <BsInstagram className="text-[#E1306C] mx-1" />
+            </li>
+          </Link>
+         
+        </ul>
+  
+    </article>
+    <article>
+      <p className='text-lg text-blue-50 font-bold'>Contactanos</p>
+      <ul className='text-blue-50 list-none '>
+        <li className='flex'>
+        <FaWhatsapp className='mt-1 me-1' />1130516831
+        </li>
+      </ul>
+    </article>
+    <div className="bg-azul-oscuro rounded-xl p-4">
         <p className="text-lg text-slate-200">
           &copy; Todos los derechos reservados
         </p>
         <p className=" text-slate-200 text-lg flex flex-row justify-center ">Hecho con <BsHeartFill className='text-fucsia m-1' /> </p>
       </div>
-    </article>
-    <article>
-      <div className="bg-white rounded-md p-4 flex items-center justify-center flex-col mt-2">
-        <p className="text-lg text-azul-oscuro font-oswald font-bold mx-1">
-          Sigueme
-        </p>
-        <ul className="flex  justify-between   text-slate-200 mt-1">
-          <Link to="https://x.com/Nicolas50418799">
-            <li>
-              <BsTwitterX className="text-[#000] mx-1" />
-            </li>
-          </Link>
-          <Link to="https://www.facebook.com/Nico.Mas777?mibextid=ZbWKwL">
-            <li>
-              <BsFacebook className="text-[#1877F2] mx-1" />
-            </li>
-          </Link>
-          <Link to='https://www.instagram.com/tucumanos2.0/'>
-            <li>
-              <BsInstagram className="text-[#E1306C] mx-1" />
-            </li>
-          </Link>
-          <Link to='https://www.linkedin.com/in/cosmenicolas/'>
-            <li>
-              <BsLinkedin className="text-[#0A66C2] mx-1" />
-            </li>
-          </Link>
-          <Link to='https://www.threads.net/@cosme_nicolas'>
-            <li>
-              <BsFillThreadsFill className="text-[#000] mx-1" />
-            </li>
-          </Link>
-        </ul>
-      </div>
-    </article>
   </footer>
   )
 }
