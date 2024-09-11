1d6af3bc8f706492d41c8bac49200443fe266d92
CosmeNicolas <nicomas7590@gmail.com>
2024-09-10 21:20:01 -0300

    agregue descipciones a la card para poder ver en el detalle

--- src/components/fragments/DetalleProducto.jsx
+++ src/components/fragments/DetalleProducto.jsx
@@ -1,39 +1,46 @@
-import React from 'react'
-import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Button, Image } from "@nextui-org/react";
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Link,
+  Button,
+  Image,
+} from "@nextui-org/react";
 
 const DetalleProducto = () => {
   return (
-    <section className='container flex flex-col lg:flex-row justify-center p-8 bg-cover bg-center error404 min-w-full min-h-screen'>
+    <section className="container flex flex-col lg:flex-row justify-center p-8 bg-cover bg-center error404 min-w-full min-h-screen">
       <Card isFooterBlurred className="col-span-12 lg:col-span-7 ">
         <CardHeader className="absolute z-10 top-1 flex-col items-start">
-          <p className="text-tiny text-white/60 uppercase font-bold">Your day your way</p>
-          <h4 className="text-white/90 font-medium text-xl">Your checklist for better sleep</h4>
+          <p className="text-tiny text-white/60 uppercase font-bold">
+            Your day your way
+          </p>
+          <h4 className="text-white/90 font-medium text-xl">
+            Your checklist for better sleep
+          </h4>
         </CardHeader>
         <Image
           removeWrapper
           alt="Relaxing app background"
-          className="z-0 w-full h-full object-cover  "
+          className="z-0 w-full h-full object-cover rounded-b-none "
           src="https://nextui.org/images/card-example-5.jpeg"
         />
-        <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-          <div className="flex flex-grow gap-2 items-center">
-            <Image
-              alt="Breathing app icon"
-              className="rounded-full w-10 h-11 bg-black"
-              src="https://nextui.org/images/breathing-app-icon.jpeg"
-            />
-            <div className="flex flex-col">
-              <p className="text-tiny text-white/60">Breathing App</p>
-              <p className="text-tiny text-white/60">Get a good night's sleep.</p>
-            </div>
-          </div>
-          <Button radius="full" size="sm">Get App</Button>
+        <CardBody>
+          <p>Make beautiful websites regardless of your design experience.</p>
+          <p>Make beautiful websites regardless of your design experience.</p>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <Button as={Link} to="/detalleProducto" color="secondary">
+            Agregar al Carrito
+          </Button>
         </CardFooter>
       </Card>
-      
-     
 
-    {/*   <Card className="bg-[#00000087] rounded-s-none rounded-t-lg lg:rounded-l-none lg:mt-0">
+      {/*   <Card className="bg-[#00000087] rounded-s-none rounded-t-lg lg:rounded-l-none lg:mt-0">
         <CardHeader className="flex gap-3">
           <Image
             alt="nextui logo"
@@ -63,7 +70,7 @@ const DetalleProducto = () => {
         </CardFooter>
       </Card> */}
     </section>
-  )
-}
+  );
+};
 
-export default DetalleProducto
+export default DetalleProducto;
