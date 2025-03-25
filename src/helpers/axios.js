779b0095f422a879349631cb15e7ce4552c70e09
CosmeNicolas <nicomas7590@gmail.com>
2025-03-24 22:00:03 -0300

    prueba front deploy3

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const  token = JSON.parse(sessionStorage.getItem('token'))
 console.log(token)
 const clienteAxios = axios.create({
-  baseURL:`${import.meta.env.VITE_URL_BACK_LOCAL}/api`
+   baseURL: import.meta.env.VITE_URL_BACK_LOCAL || 'https://back-mayorista.vercel.app/api'
 })
 /* configuracion de la cabecera para el token */
 export const configHeaders = {
